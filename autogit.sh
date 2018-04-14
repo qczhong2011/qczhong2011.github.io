361267bb59a76cfbbddf10330b74475c1fed0bc8
qichao.zhong <qczhong2011@gmail.com>
2018-04-14 09:50:20 +0800

    update autogit.sh 3

--- autogit.sh
+++ autogit.sh
@@ -1,14 +1,28 @@
 #! /bin/sh
+
 git add .
+
 if [ x"$1" != x ]; then
     git commit -m "$1"
 else
     git commit -m "update post"
 fi
-echo "----push to origin hexo----"
+
+echo "----------------------------------------------"
+echo "----------push to origin hexo-----------------"
+echo "----------------------------------------------"
 git push origin hexo
-echo "----goto server to gen----"
+
+
+if [ x"$2" != x ]; then
+	echo "Just want to push Config"
+	exit 0
+fi
+echo "----------------------------------------------"
+echo "----------ssh to server autohexo--------------"
+echo "----------------------------------------------"
 ssh git@120.79.138.59 /home/git/autohexo.sh
-echo "--------------------------"
-echo "-------auto hexo end------"
-echo "--------------------------"
+
+echo "----------------------------------------------"
+echo "----------------auto hexo end-----------------"
+echo "----------------------------------------------"
