a38648e6f0328d1471c6c893afa093935acc42a6
qichao.zhong <qczhong2011@gmail.com>
2018-04-14 09:43:28 +0800

    update

--- autogit.sh
+++ autogit.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 git add .
-if [ x$1 != x ]; then
-    git commit -m "$1"
+if [ x"$1" != x ]; then
+    git commit -m $1
 else
-	git commit -m "update post"
+    git commit -m "update post"
 fi
+echo "----push to origin hexo----"
 git push origin hexo
+exit 1
+echo "----goto server to gen----"
 ssh git@120.79.138.59 /home/git/autohexo.sh
 echo "---------------------"
 echo "----auto hexo end----"
