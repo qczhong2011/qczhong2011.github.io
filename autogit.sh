04767924516c7616ecc2556abad915779e5815f3
qichao.zhong <qczhong2011@gmail.com>
2018-04-14 09:45:52 +0800

    update autogit.sh too

--- autogit.sh
+++ autogit.sh
@@ -7,9 +7,8 @@ else
 fi
 echo "----push to origin hexo----"
 git push origin hexo
-exit 1
 echo "----goto server to gen----"
 ssh git@120.79.138.59 /home/git/autohexo.sh
-echo "---------------------"
-echo "----auto hexo end----"
-echo "---------------------"
+echo "--------------------------"
+echo "-------auto hexo end------"
+echo "--------------------------"
