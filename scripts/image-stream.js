7d2f1b02a77b5c41ef3eab3603731937f01ae6f8
qichao.zhong <qczhong2011@gmail.com>
2018-04-21 10:39:19 +0800

    update image-stream.js

--- scripts/image-stream.js
+++ scripts/image-stream.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var config = {
-  'jquery': '//cdn.bootcss.com/jquery/2.1.0/jquery.min.js',
-  'jquery_lazyload': '//cdn.bootcss.com/jquery.lazyload/1.9.1/jquery.lazyload.min.js',
+  'jquery': '//cdn.bootcss.com/jquery/2.1.5/jquery.min.js',
+  'jquery_lazyload': '//cdn.bootcss.com/jquery.lazyload/1.9.7/jquery.lazyload.min.js',
   'img_placeholder': 'https://ws4.sinaimg.cn/large/e724cbefgw1etyppy7bgwg2001001017.gif'
 }
 
@@ -24,7 +24,7 @@ hexo.extend.tag.register('stream', function(args, content){
   }
   result += '<div class="hexo-img-stream">';
   result += '<style type="text/css">';
-  result += '.hexo-image-steam-lazy {display:block;}.hexo-img-stream{width:95%;max-width:1100px;margin:3% auto}div.hexo-img-stream figure{background:#fefefe;box-shadow:0 1px 2px rgba(34,25,25,0.4);margin:0 0.05% 3%;padding:3%;padding-bottom:10px;display:inline-block;max-width:26%}div.hexo-img-stream figure img{border-bottom:1px solid #ccc;padding-bottom:15px;margin-bottom:5px}div.hexo-img-stream figure figcaption{font-size:.9rem;color:#444;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}div.hexo-img-stream small{font-size:1rem;float:right;text-transform:uppercase;color:#aaa}div.hexo-img-stream small a{color:#666;text-decoration:none;transition:.4s color}@media screen and (max-width:750px){.hexo-img-stream{column-gap:0}}';
+  result += '.hexo-image-steam-lazy {display:block;}.hexo-img-stream{width:100%;margin:3% auto}div.hexo-img-stream figure{background:#fefefe;box-shadow:0 1px 2px rgba(34,25,25,0.4);margin:0 0.05% 3%;padding:3%;padding-bottom:10px;display:inline-block;max-width:25%}div.hexo-img-stream figure img{border-bottom:1px solid #ccc;padding-bottom:3px;margin-bottom:5px}div.hexo-img-stream figure figcaption{font-size:.9rem;color:#444;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}div.hexo-img-stream small{font-size:1rem;float:right;text-transform:uppercase;color:#aaa}div.hexo-img-stream small a{color:#666;text-decoration:none;transition:.4s color}@media screen and (max-width:750px){.hexo-img-stream{column-gap:0}}';
   result += '</style>';
   result += content;
   result += '</div>';
--- source/photos/index.md
+++ source/photos/index.md
@@ -5,23 +5,20 @@ type: photos
 comments: false
 ---
 
-<!-- 网易云音乐 起风了 手动播放 -->
-<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=461525011&auto=0&height=66"></iframe>
-
 {% stream %}
-{% figure 
+{% figure
 https://zingqi.oss-cn-shenzhen.aliyuncs.com/1.yunnandali/1.%e5%a4%a7%e7%90%86%e9%97%a8.JPG
 [云南大理](/photos-yunnandali/) 
 %}  
-{% figure 
+{% figure
 https://zingqi.oss-cn-shenzhen.aliyuncs.com/2.balidao/1.%e5%a5%87%e7%9f%b3.JPG
 [巴厘岛](/photos-balidao/) 
 %}  
-{% figure   
+{% figure
 https://zingqi.oss-cn-shenzhen.aliyuncs.com/3.honghuahu/1.%e7%ba%a2%e8%8a%b1%e5%be%ae%e7%99%bd.jpg
 [惠州红花湖 ](/photos-honghuahu/) 
 %}   
-{% figure 
+{% figure
 https://zingqi.oss-cn-shenzhen.aliyuncs.com/4.szyumaoqiu/1.%e8%93%84%e5%8a%bf%e4%bb%a5%e5%be%85.jpg
 [深圳羽毛球](/photos-szyumaoqiu/) 
 %}   
